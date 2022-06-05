d9b786e7ed7cbfae955ca5d639bc1ed9d492bd7e
ketjehdev <ketjeh.developer@gmail.com>
2022-06-06 00:20:17 +0700

    user update

--- user/index.php
+++ user/index.php
@@ -1,8 +1,172 @@
 <?php
+
+include '../koneksi.php';
+
 session_start();
+
 
 // cek apakah user sudah login sesuai role apa blom
 if ($_SESSION["role"] != "user") {
     // alihkan ke halaman login
     header('Location:../index.php');
 }
+
+// sql select all data
+$sql = "SELECT * FROM menu";
+// query all data users with connection database
+$query = mysqli_query($conn, $sql);
+
+// sql select rekap
+$sql_menu = "SELECT * FROM rekap";
+// query all data users with connection database
+$query_menu = mysqli_query($conn, $sql_menu);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Dashboard | Bersihin</title>
+</head>
+<style>
+    nav {
+        background: red;
+        width: 100%;
+        bottom: 0;
+        background: #fff;
+        box-shadow: 1px 1px 1px 1px rgba(0, 0, 0, 0.7);
+        position: fixed;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        padding: 5px 20px 5px 20px;
+        z-index: 1;
+    }
+
+    nav a {
+        text-decoration: none;
+        color: navy;
+    }
+
+    nav a .aktif {
+        color: red;
+    }
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
+</style>
+
+<body class="bg-light">
+
+    <nav>
+        <div class="d-flex flex-column align-items-center" title="Home">
+            <a href="./index.php">
+                <i data-feather="home" class="aktif"></i>
+            </a>
+            <span style="font-size: 10px;">Home</span>
+        </div>
+
+        <div class="d-flex flex-column align-items-center" title="Home">
+            <a href="./index.php">
+                <i data-feather="file-text"></i>
+            </a>
+            <span style="font-size: 10px;">Transaksi</span>
+        </div>
+
+        <div class="d-flex flex-column align-items-center" title="Home">
+            <a href="../logout.php">
+                <i data-feather="log-out"></i>
+            </a>
+            <span style="font-size: 10px;">Keluar</span>
+        </div>
+
+    </nav>
+
+
+    <div class="container-fluid">
+        <div class="row d-flex flex-column align-items-center justify-content-center text-light" style="background: navy; height: 20vh;">
+            <div class="col-12">
+                <h4 class="mb-0">Hai, <?= $_SESSION['username'] ?></h4>
+                <p>Selamat datang di Bersihin</p>
+            </div>
+        </div>
+
+        <div class="row p-3">
+            <div class="col-12">
+                <h4>Dashboard</h4>
+                <div class="card mb-4">
+                    <div class="card-body d-flex justify-content-evenly">
+                        <?php
+                        while ($data = mysqli_fetch_array($query)) {
+                        ?>
+                            <div class="d-flex flex-column align-items-center">
+                                <a href="menu.php?nama_menu=<?= $data['nama_menu'] ?>">
+                                    <img src="../img/logo.png" style="width: 80px; height: 80px;">
+                                </a>
+                                <?php
+                                if ($data['nama_menu'] == 'Bersihin Aja') {
+                                    echo "<span class='px-3 mx-2 bg-primary text-light text-center' style='border-radius:12px'>" . $data['nama_menu'] . "</span>";
+                                } else if ($data['nama_menu'] == 'Bersihin Kilat') {
+                                    echo "<span class='px-3 mx-2 bg-danger text-light text-center' style='border-radius:12px'>" . $data['nama_menu'] . "</span>";
+                                }
+                                ?>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="card-header d-flex p-3 justify-content-between alert-info">
+                        <h6 class="mb-0">History Transaksi</h6>
+                        <a href="" style="text-decoration: none;">View All</a>
+                    </div>
+                    <div class="card-body mb-3">
+                        <div class="table-responsive">
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr class="text-center" style="white-space: nowrap;">
+                                        <th>No.</th>
+                                        <th>Layanan</th>
+                                        <th>Berat</th>
+                                        <th>Harga</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php $no = 1; ?>
+                                    <?php
+                                    while ($data = mysqli_fetch_array($query_menu)) {
+                                    ?>
+                                        <tr class="text-center">
+                                            <td><?= $no++; ?></td>
+                                            <td><?= $data['layanan'] ?></td>
+                                            <td><?= $data['berat'] ?></td>
+                                            <td><?= $data['harga'] ?></td>
+                                            <td><?= $data['status'] ?></td>
+                                        </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <script>
+        feather.replace()
+    </script>
+</body>
+
+</html>
---
+++ user/menu.php
@@ -0,0 +1,117 @@
+<?php
+
+include '../koneksi.php';
+
+session_start();
+
+// cek apakah user sudah login sesuai role apa blom
+if ($_SESSION["role"] != "user") {
+    // alihkan ke halaman login
+    header('Location:../index.php');
+}
+
+$nama_menu = $_GET['nama_menu'];
+$result = mysqli_query($conn, "SELECT * FROM menu WHERE nama_menu='$nama_menu'");
+
+while ($data = mysqli_fetch_assoc($result)) {
+    $nama_menu = $data['nama_menu'];
+    $harga = $data['harga'];
+}
+
+if (isset($_POST['submit'])) {
+    $pemesan = $_POST['pemesan'];
+    $no_telepon = $_POST['no_telepon'];
+    $layanan = $_POST['layanan'];
+    $berat = $_POST['berat'];
+    $address = $_POST['address'];
+    $status = 'on progress';
+
+    $sql = "INSERT INTO rekap(pemesan, no_telepon, layanan, berat, address, status) 
+            VALUES('$pemesan', '$no_telepon', '$layanan', '$berat', '$address', '$status')";
+
+    $result = mysqli_query($conn, $sql);
+
+    header('Location: ./index.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title><?= $nama_menu; ?> | Bersihin</title>
+</head>
+<style>
+    ::-webkit-scrollbar {
+        display: none;
+    }
+</style>
+
+<body class="bg-light">
+
+    <div class="container-fluid">
+        <div class="row d-flex flex-column align-items-center justify-content-center text-light" style="background: navy; height: 20vh;">
+            <div class="col-12">
+                <h4 class="mb-0"><?= $nama_menu; ?></h4>
+                <p>Layanan</p>
+            </div>
+        </div>
+
+        <div class="row p-3">
+            <div class="col-12">
+                <h4></h4>
+                <div class="card">
+                    <div class="card-header d-flex p-3 align-items-center justify-content-between alert-info">
+                        <h6 class="mb-0">Order Service</h6>
+                        <button class="btn btn-success">
+                            Rp. <?= number_format($harga, 0, ',', '.'); ?>
+                        </button>
+                    </div>
+
+                    <div class="card-body mb-3">
+                        <form action="" method="POST">
+                            <input type="hidden" name="harga" value="<?= $harga; ?>">
+                            <label for="Pemesan">Pemesan :</label>
+                            <input type="text" name="pemesan" class="form-control mb-2" id="pemesan" readonly value="<?= $_SESSION['username'] ?>">
+
+                            <label for="no_telepon">No. Telepon :</label>
+                            <input type="text" name="no_telepon" class="form-control mb-2" id="no_telepon" readonly value="<?= $_SESSION['no_telepon'] ?>">
+
+                            <label for="layanan">Layanan :</label>
+                            <input type="text" name="layanan" class="form-control mb-2" id="layanan" readonly value="<?= $nama_menu; ?>">
+
+                            <label for="berat">Berat :</label>
+                            <?php
+                            if ($nama_menu == 'Bersihin Aja') {
+                                echo '<input type="text" name="berat" class="form-control mb-2" id="berat" readonly value="< 5 KG">';
+                            } else {
+                                echo '<input type="text" name="berat" class="form-control mb-2" id="berat" readonly value="< 10 KG">';
+                            }
+                            ?>
+
+                            <label for="address">Address :</label>
+                            <input type="text" name="address" class="form-control mb-2" id="address" readonly value="<?= $_SESSION['address'] ?>">
+
+                            <button type="submit" name="submit" class="btn btn-success mt-3" style="width: 100%;">Pesan</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+    <script src="https://unpkg.com/feather-icons"></script>
+
+    <script>
+        feather.replace()
+    </script>
+</body>
+
+</html>
