a7a90725be3832883c14d4b0255946f85e67b4a1
ketjehdev <ketjeh.developer@gmail.com>
2022-06-09 20:07:11 +0700

    update data

--- admin/transaksi.php
+++ admin/transaksi.php
@@ -23,7 +23,7 @@ $result = mysqli_query($conn, 'SELECT * FROM rekap');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
-    <title>Dashboard | Bersihin</title>
+    <title>Transaksi | Bersihin</title>
 </head>
 <style>
     nav {
